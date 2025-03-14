c390631ab27b960534d8333b35e270c0fd81098c
Udhayavarshini19 <udhayavarshini04@gmail.com>
2025-03-14 13:06:10 +0530

    updated

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('xyz bank customer page', () => {
+  beforeEach(()=>{
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/list');
   })
+  it('validate customer existance', () => {
+    cy.get('table>tbody>tr').should('have.length.gt',0);
+  })
+  it('should fetch searched customer',()=>{
+    cy.get('input[ng-model="searchCustomer"]').type("harry");
+    cy.get('table>tbody>tr>td').should('contain','Harry');
+  })
+  it('should delete customer',()=>{
+    cy.get('table>tbody>tr>td').contains('Harry').parent().find('button').click();
+    cy.get('table>tbody>tr>td').should('not.contain','Harry');
+  })
+  it('should delete customer fetched by search',()=>{
+    cy.get('input[ng-model="searchCustomer"]').type("harry");
+    cy.get('table>tbody>tr>td').contains('Harry').parent().find('button').click();
+    cy.get('table>tbody>tr').should('have.length',0);
+  })
+
 })
+
+
+
+
