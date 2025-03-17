e9ffe3d0c908e81e9f6167ad02036abd1735f538
Udhayavarshini19 <udhayavarshini04@gmail.com>
2025-03-17 22:36:04 +0530

    egfsD

--- cypress.config.js
+++ cypress.config.js
@@ -1,41 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      quiet: true,
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
+  screenshotsFolder: "cypress/reports/mochareports/assets",
+
+  // Cypress test settings
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
+      
+      return config;
     },
-    baseUrl: 'http://localhost:8080', // Set your application's base URL
-    retries: 2, // Retry failed tests up to 2 times
-    video: true, // Enable video recording on test run
-    screenshotOnRunFailure: true, // Automatically capture screenshots on test failure
-    chromeWebSecurity: false, // Disable Chrome web security to prevent cross-origin issues
-    defaultCommandTimeout: 10000, // Increase timeout for commands
-    env: {
-      loginUrl: '/login', // Example of setting environment variables
-    },
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    specPattern: "cypress/e2e/**/*.cy.js", // Set the test file pattern
-
-    // Mochawesome reporter settings
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/reports",
-      overwrite: false,
-      html: true,
-      json: true
-    },
-
-    // Screenshot options (more customization)
-    screenshot: {
-      // Set whether to take a screenshot on failed tests
-      onFail: true, // This option is the same as `screenshotOnRunFailure: true`
-      capture: "runner", // You can set this to "viewport", "runner", or "both" for different screenshot behaviors
-      // Specify screenshot path (defaults to cypress/screenshots)
-      screenshotFolder: "cypress/screenshots", // Ensure this folder exists or Cypress will create it
-      // Customize the screenshot naming pattern
-      fileName: "[name]-[failure-type]-[timestamp].png", // Add more variables if needed
-    }
-  }
+    specPattern: "cypress/e2e/**/*.cy.js",
+  },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,11 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import addContext from "mochawesome/addContext";
+
+Cypress.on("test:after:run", (test, runnable) => {
+  if (test.state === "failed") {
+    const screenshot = `assets/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
+});
